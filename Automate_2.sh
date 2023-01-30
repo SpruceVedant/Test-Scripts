70b41ba013ab517ea109f70806dd861ba00abc67
Vedant <vedantbakshi07@gmial.com>
2023-01-30 16:25:49 +0530

    Added

---
+++ Automate_2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define task1 function
+task1() {
+  # rename all jpeg files in the current directory to have a .jpg extension
+  for file in *.jpeg; do
+    mv "$file" "${file%.jpeg}.jpg"
+  done
+  echo "Task 1: JPEG files renamed."
+}
+
+# Define task2 function
+task2() {
+  # move all files in the Downloads folder to the Documents folder
+  mv ~/Downloads/* ~/Documents/
+  echo "Task 2: Files moved from Downloads to Documents."
+}
+
+# Define task3 function
+task3() {
+  # delete all files in the trash
+  rm -rf ~/.local/share/Trash/*
+  echo "Task 3: Trash emptied."
+}
+
+# Call functions to run tasks
+task1
+task2
+task3
+
+# End of script
